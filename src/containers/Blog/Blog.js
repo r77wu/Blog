492d10e1a31bc3c466103342915115f1acdef87c
Ricky <ricarowu@gmail.com>
2020-08-27 12:08:27 -0400

    add package file

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import Post from '../../components/Post/Post';
+import FullPost from '../../components/FullPost/FullPost';
+import NewPost from '../../components/NewPost/NewPost';
+import './Blog.css';
+
+class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPostId: null
+    }
+
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+            const posts = response.data.slice(0, 4);
+            const updatedPosts = posts.map(post => {
+                return {
+                    ...post,
+                    author: 'Max'
+                }
+            })
+            this.setState({posts: updatedPosts });
+        }).catch(err => {
+            alert(err);
+        });
+        
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id})
+    }
+
+    render () {
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title} author={post.author} clicked={() => this.postSelectedHandler(post.id)}/>
+        })
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <section>
+                    <FullPost id={this.state.selectedPostId}/>
+                </section>
+                <section>
+                    <NewPost />
+                </section>
+            </div>
+        );
+    }
+}
+
+export default Blog;
